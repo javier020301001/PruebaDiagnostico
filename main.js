724cba73ad05711b4e495e3852d5cbdffca5d8c9
Javier020301 <javiesaransig2@gmail.com>
2025-10-26 19:20:26 -0500

    Version tres, prueba diagnostico, creacion desktop con electron

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello world")
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
